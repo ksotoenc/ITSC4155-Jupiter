93629e0b1f3f4e13f1a160f2550b1d00dab837c3
amoua43 <148017124+amoua43@users.noreply.github.com>
2025-02-28 19:46:57 -0500

    Updated once more

--- registration_tracker/app/push_branch.py
+++ registration_tracker/app/push_branch.py
@@ -2,3 +2,5 @@ import streamlit as st
 st.title("Hello, World!")
 st.write("This is a test file for the Git repository")
 st.write("This is the modified test/file for the Git repository")
+
+st.write("This is for the assignment")
