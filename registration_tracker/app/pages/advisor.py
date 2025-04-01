2e15d966b088ba6b7b7eab74a2f6d3e307175b55
amoua43 <148017124+amoua43@users.noreply.github.com>
2025-04-01 16:11:43 -0400

    Advisor Updated

--- registration_tracker/app/pages/advisor.py
+++ registration_tracker/app/pages/advisor.py
@@ -4,3 +4,13 @@ from app_utils import get_student, get_student_plan
 
 st.title("Advisor Dashboard 🏫")
 st.write("Here, advisors can manage and review student plans.")
+
+option = st.selectbox("Select a student",("Student 1", "Student 2", "Student 3"))
+
+st.header("Student: {Name}")
+st.header("Expected Graduation Date{Date}")
+st.header("Major {Major}")
+st.header("GPA {GPA}")
+
+st.button("View Current Plan")
+st.button("View Feedback")
